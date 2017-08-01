70cb4c31f8b4e2ce9acf9794815194acde67080a
voobscout <voobscout@archlinux.info>
2017-08-01 12:51:19 +0200

    ..

--- scripts/copy_prep.rb
+++ scripts/copy_prep.rb
@@ -4,9 +4,9 @@ cmd = "docker run -dti -v /mnt/images:/mnt/images -v /opt/tmp:/opt/tmp sdwandemo
 uri = "https://raw.githubusercontent.com/sdwandemo/topology2/master/docker-compose.yml"
 compose_file = YAML.load(open(uri).read)
 images = compose_file['services'].select{|k,v| v['environment'] && v['environment']['VM_DISK']}
-src_dst = images.map{|k,v|  "/opt/tmp/#{Pathname.new(v['environment']['VM_DISK']).basename.to_s} /mnt/images/_#{Pathname.new(v['environment']['VM_DISK']).basename.to_s.gsub('.qcow2', '')}-#{k}.qcow2"}
 
+src_dst = images.map{|k,v|  "/opt/tmp/#{Pathname.new(v['environment']['VM_DISK']).basename.to_s} /mnt/images/_#{Pathname.new(v['environment']['VM_DISK']).basename.to_s.gsub('.qcow2', '')}-#{k}.qcow2"}
 sc = ['#!/bin/bash']
-src_dst.each { |i| sc << "#{cmd} cp #{i} && touch #{i.split(' ').last}_" }
+src_dst.each { |i| sc << "#{cmd} cp #{i} && touch #{i.split(' ').last}.lock" }
 Pathname.new('/opt/tmp/init_images').write(sc.join("\n"))
 FileUtils.chmod(0755, '/opt/tmp/init_images')
